152ed133f098a8c05c133d2ee0240abb291d2b27
mturalija <marinamiochr@gmail.com>
2024-11-13 16:48:20 +0100

    Vjezba tablica zivotinja

---
+++ Vjezba zooSQLQuery1.sql
@@ -0,0 +1,17 @@
+--create date base zoo;
+
+--drop datebase zoo,
+
+--use zoo;
+
+--create table ( zivotinja,
+--sifra int,
+--vrsta varchar(50),
+--ime varchar (50),
+--djelatnik int,
+--prostorija int,
+--datum int,
+--);
+
+
+
