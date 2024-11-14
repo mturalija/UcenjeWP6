8390236899056d2c6c7d8ce70a4fb3e7402979a9
mturalija <marinamiochr@gmail.com>
2024-11-14 08:52:46 +0100

    Kreiranje baze sport

---
+++ Kreiranje baze sportSQLQuery1.sql
@@ -0,0 +1,13 @@
+--create database sport
+--drop database sport
+--use sport;
+--create table( igrac
+--sifra int
+--ime varchar(50),
+--prezime varchar (50),
+--datum_rodjenja datatime,
+--pozicija datetime,
+--broj dresa datatime,
+--klub (varchar),
+
+
