6a7a2da41f52b27ddb290d17a99af4b521e8ec09
mturalija <marinamiochr@gmail.com>
2024-11-21 19:06:25 +0100

    Update skriptawp6.sql

--- skriptawp6.sql
+++ skriptawp6.sql
@@ -6,7 +6,7 @@ go -- dajemo mu vremena da se prebaci prije nego što ide dalje
 drop database if exists edunovawp6;
 go
 -- kreiram novu bazu
-create database edunovawp6 collate Croatian_CI_AS
+create database edunovawp6 collate Croatian_CI_AS;
 go
 -- pozicioniram se na bazu
 use edunovawp6;
@@ -47,27 +47,31 @@ grupa int not null references grupe(sifra),
 polaznik int not null references polaznici(sifra)
 );
 
---1
-insert into smjerovi (naziv,cijena,izvodiseod,vaucer)
-values ('web programiranje', 1225.48,'2024-11-06 17:00', 1);
 
-insert into smjerovi (naziv, cijena, izvodiseod,vaucer)
+-- 1
+insert into smjerovi (naziv, cijena, izvodiseod, vaucer)
+values ('Web programiranje',1225.48,'2024-11-06 17:00',1);
+
+insert into smjerovi(naziv, cijena, izvodiseod, vaucer)
 values
---2
+-- 2
 ('Java programiranje',null,null,null),
---3
-('serviseri',800, '2020-01-01',0);
+-- 3
+('Serviseri',800,'2020-01-01',0);
+
 
 insert into grupe(naziv,velicinagrupe,smjer)
 values
---1
+-- 1
 ('WP6',27,1),
---2
+-- 2
 ('WP5',27,1),
---3
+-- 3
 ('WP7',27,1),
---4
+-- 4
 ('JP17',11,3);
+
+
 
 INSERT INTO polaznici (ime, prezime, email) VALUES 
 ('Marko', 'Radoš', 'rados.marko05@gmail.com'),
@@ -95,8 +99,8 @@ INSERT INTO polaznici (ime, prezime, email) VALUES
 ('Ivan', 'Režan', 'forexivanrezan@gmail.com'),
 ('Antonio', 'Simpf', 'antonijosimpf@gmail.com');
 
-insert into clanovi (grupa,polaznik)
-values(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),
-(1,8),(1,9),(1,10),(1,11),(1,12),(1,13),(1,14),(1,15),
-(1,16),(1,17),(1,18),(1,19),(1,20),(1,21),(1,22),
-(1,23),(1,24);
+insert into clanovi (grupa, polaznik)
+values (1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7)
+,(1,8),(1,9),(1,10),(1,11),(1,12),(1,12),(1,14),(1,15)
+,(1,16),(1,17),(1,18),(1,19),(1,20),(1,21),(1,22),(1,23)
+,(1,24);
