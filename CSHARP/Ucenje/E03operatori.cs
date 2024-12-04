ea3deb183824ca472818cbc5c47b84d770025f84
mturalija <marinamiochr@gmail.com>
2024-12-04 18:42:59 +0100

    Nastavak E03 operatori

--- CSHARP/Ucenje/E03operatori.cs
+++ CSHARP/Ucenje/E03operatori.cs
@@ -17,6 +17,48 @@ namespace Ucenje
             int k = 2, l = 1;
 
             Console.WriteLine( l/k);
+            Console.WriteLine( l/(float)k);
+
+
+            Console.WriteLine(i!=k);
+
+            Console.WriteLine( "5%2={0}", 5% 2);
+
+            Console.WriteLine( "4%2={0}", 4%2);
+
+
+            int b = 0;
+
+            b = b + 1;
+
+            b += 1;
+
+            b++;
+
+            Console.WriteLine(b);
+
+
+            b = 0;
+
+            Console.WriteLine( b++);
+
+            Console.WriteLine( ++b);
+
+            int t = 0, c = 1;
+            t = c++ + t;
+            c += ++t;
+            Console.WriteLine((c + t); ;
+
+
+
+
+
+
+
+
+
+
+
         }
 
-}
+
